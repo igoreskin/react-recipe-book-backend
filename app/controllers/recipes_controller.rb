3f053bb00442e10ba2fc01b161f6b3bebebd1522
Igor Eskin <github email address>
2018-05-15 17:23:00 -0400

    Delete button added

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,6 +10,12 @@ class RecipesController < ApplicationController
     render json: @recipe
   end
 
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update_attributes(recipe_params)
+    render json: @recipe
+  end
+
   private
 
   def recipe_params
